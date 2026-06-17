0adc25e22782405591fb5c57b698e247bb8cd77e
Sebastian Olejnik <259274@student.pwr.edu.pl>
2026-06-17 10:37:40 +0200

    Add staticMap + map & vector test

---
+++ lib/StaticMap/staticMap.h
@@ -0,0 +1,118 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
+
+template <typename Key, typename Value, std::size_t N> class static_map {
+  private:
+    std::array<std::pair<Key, Value>, N> data{};
+    std::size_t count = 0;
+
+  public:
+    constexpr std::size_t size() const noexcept {
+        return count;
+    }
+
+    constexpr std::size_t capacity() const noexcept {
+        return N;
+    }
+
+    constexpr bool empty() const noexcept {
+        return count == 0;
+    }
+
+    bool contains(const Key &key) const noexcept {
+        return find(key) != end();
+    }
+
+    Value &at(const Key &key) {
+        auto it = find(key);
+        if (it == end()) {
+            throw std::out_of_range("key not found");
+        }
+        return it->second;
+    }
+
+    const Value &at(const Key &key) const {
+        auto it = find(key);
+        if (it == end()) {
+            throw std::out_of_range("key not found");
+        }
+        return it->second;
+    }
+
+    Value &operator[](const Key &key) {
+        auto it = find(key);
+        if (it != end()) {
+            return it->second;
+        }
+
+        if (count >= N) {
+            throw std::length_error("static_map is full");
+        }
+
+        data[count] = {key, Value{}};
+        return data[count++].second;
+    }
+
+    void insert(const Key &key, const Value &value) {
+        auto it = find(key);
+
+        if (it != end()) {
+            it->second = value; // aktualizacja istniejącego wpisu
+            return;
+        }
+
+        if (count >= N) {
+            throw std::length_error("static_map is full");
+        }
+
+        data[count++] = {key, value};
+    }
+
+    void erase(const Key &key) {
+        for (std::size_t i = 0; i < count; ++i) {
+            if (data[i].first == key) {
+                data[i] = std::move(data[count - 1]);
+                --count;
+                return;
+            }
+        }
+    }
+
+    auto find(const Key &key) noexcept {
+        for (std::size_t i = 0; i < count; ++i) {
+            if (data[i].first == key) {
+                return data.begin() + i;
+            }
+        }
+        return end();
+    }
+
+    auto find(const Key &key) const noexcept {
+        for (std::size_t i = 0; i < count; ++i) {
+            if (data[i].first == key) {
+                return data.begin() + i;
+            }
+        }
+        return end();
+    }
+
+    auto begin() noexcept {
+        return data.begin();
+    }
+
+    auto end() noexcept {
+        return data.begin() + count;
+    }
+
+    auto begin() const noexcept {
+        return data.begin();
+    }
+
+    auto end() const noexcept {
+        return data.begin() + count;
+    }
+};
---
+++ test/test_native/staticVector.cpp
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+
+#include "staticVector.h"
+
+TEST(StaticVector, EmptyVector) {
+    static_vector<int, 8> vec;
+
+    EXPECT_EQ(vec.size(), 0);
+    EXPECT_EQ(vec.capacity(), 8);
+}
+
+TEST(StaticVector, PushBack) {
+    static_vector<int, 8> vec;
+
+    vec.push_back(10);
+    vec.push_back(20);
+    vec.push_back(30);
+
+    EXPECT_EQ(vec.size(), 3);
+
+    EXPECT_EQ(vec[0], 10);
+    EXPECT_EQ(vec[1], 20);
+    EXPECT_EQ(vec[2], 30);
+}
+
+TEST(StaticVector, RangeForIteration) {
+    static_vector<int, 8> vec;
+
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    int sum = 0;
+
+    for (auto v : vec) {
+        sum += v;
+    }
+
+    EXPECT_EQ(sum, 6);
+}
+
+TEST(StaticVector, ThrowsWhenFull) {
+    static_vector<int, 2> vec;
+
+    vec.push_back(1);
+    vec.push_back(2);
+
+    EXPECT_THROW(vec.push_back(3), std::length_error);
+}
+
+TEST(StaticVector, MovePushBack) {
+    static_vector<std::string, 2> vec;
+
+    std::string str = "hello";
+
+    vec.push_back(std::move(str));
+
+    EXPECT_EQ(vec[0], "hello");
+}
