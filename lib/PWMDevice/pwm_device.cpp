d1727d6101f221a50d081ac0ba8a2b0b113a3d26
Wojciech Zarzycki <127706952+wojtek-00@users.noreply.github.com>
2026-06-19 16:51:54 +0200

    [REFACTOR] implement IPWM interface

---
+++ lib/PWMDevice/pwm_device.cpp
@@ -0,0 +1,20 @@
+#include "pwm_device.h"
+
+PWMDevice::PWMDevice(const uint8_t addr, TwoWire &i2c) : pwmDriver_(addr, i2c) {};
+
+bool PWMDevice::begin() {
+    return pwmDriver_.begin();
+}
+
+void PWMDevice::setPWMFreq(float freq) {
+    pwmDriver_.setPWMFreq(freq);
+    ;
+}
+
+uint16_t PWMDevice::getPWM(uint8_t channel) {
+    return pwmDriver_.getPWM(channel, true);
+}
+
+void PWMDevice::setPWM(uint8_t channel, uint16_t val) {
+    pwmDriver_.setPin(channel, val, false);
+}
--- test/test_hardware/test_LedDriver_lib.cpp
+++ test/test_hardware/test_LedDriver_lib.cpp
@@ -1,20 +1,28 @@
-#include "LedDriver.h"
+#include "pwm_device.h"
 #include <Arduino.h>
 #include <gtest/gtest.h>
 
-class LedDriverTestBase : public ::testing::Test {
-  protected:
-    LedDriver *ledDriver = nullptr;
+bool wireInitialized = false;
 
-    void SetUp() override {
+void initWire() {
+    if (!wireInitialized) {
         Wire.setPins(13, 14);
         Wire.begin();
+        wireInitialized = true;
+    }
+}
+class LedDriverTestBase : public ::testing::Test {
+  protected:
+    PWMDevice *ledDriver = nullptr;
+
+    void SetUp() override {
+        initWire();
     }
 
     void TearDown() override {
         if (ledDriver != nullptr) {
             for (int i = 0; i < 16; i++) {
-                ledDriver->setPWM(i, 0, 0);
+                ledDriver->setPWM(i, 0);
             }
             delete ledDriver;
             ledDriver = nullptr;
@@ -22,54 +30,66 @@ class LedDriverTestBase : public ::testing::Test {
     }
 };
 
-// Klasa z automatyczną inicjalizacją - dla większości testów
-class LedDriverTestWithInit : public LedDriverTestBase {
+class LedDriverTestWithInit : public ::testing::Test {
   protected:
-    LedDriver *ledDriver = nullptr;
-
+    PWMDevice *ledDriver = nullptr;
     void SetUp() override {
-        // Najpierw inicjalizujemy I2C
-        LedDriverTestBase::SetUp();
-
-        // Potem tworzymy i inicjalizujemy driver
-        ledDriver = new LedDriver(0x40, Wire);
+        initWire();
+        ledDriver = new PWMDevice(0x40, Wire);
         bool init = ledDriver->begin();
         ASSERT_TRUE(init) << "Inicjalizacja nie powiodła się!";
 
         ledDriver->setPWMFreq(1000);
 
         for (int i = 0; i < 16; i++) {
-            ledDriver->setPWM(i, 0, 0);
+            ledDriver->setPWM(i, 0);
         }
         delay(10);
+    }
+
+    void TearDown() override {
+        if (ledDriver != nullptr) {
+            for (int i = 0; i < 16; i++) {
+                ledDriver->setPWM(i, 0);
+            }
+            delete ledDriver;
+            ledDriver = nullptr;
+        }
     }
 };
 
 TEST_F(LedDriverTestBase, TestInit) {
-    ledDriver = new LedDriver(0x40, Wire);
+    ledDriver = new PWMDevice(0x40, Wire);
     bool init = ledDriver->begin();
     EXPECT_TRUE(init);
 }
 
 TEST_F(LedDriverTestBase, TestInitError) {
-    ledDriver = new LedDriver(0x50, Wire);
+    ledDriver = new PWMDevice(0x50, Wire);
     bool init = ledDriver->begin();
     EXPECT_FALSE(init);
 }
 
 TEST_F(LedDriverTestWithInit, SetPWM) {
-    ledDriver->setPWM(0, 0, 2048);
+    ledDriver->setPWM(0, 2048);
     delay(10);
-    EXPECT_EQ(2048, ledDriver->getPWM(0, true));
+    EXPECT_EQ(2048, ledDriver->getPWM(0));
+}
+
+TEST_F(LedDriverTestWithInit, SetPWM_2) {
+    ledDriver->setPWM(4, 1000);
+    delay(10);
+    EXPECT_EQ(1000, ledDriver->getPWM(4));
 }
 
 TEST_F(LedDriverTestWithInit, MultipleChannels) {
     for (int i = 0; i < 16; i++) {
-        ledDriver->setPWM(i, 0, i * 256);
+        delay(50);
+        ledDriver->setPWM(i, i + 1);
     }
-    delay(10);
+    delay(1000);
 
     for (int i = 0; i < 16; i++) {
-        EXPECT_EQ(i * 256, ledDriver->getPWM(i, true));
+        EXPECT_EQ(i + 1, ledDriver->getPWM(i));
     }
 }
